1547aaf1d895cc69fc5630b6d9ccc63e8896b78e
Felipe Maccheri <felipemaccheri@gmail.com>
2020-02-24 14:18:06 -0300

    Adding eslint and prettier to the project

--- src/routes.js
+++ src/routes.js
@@ -2,8 +2,6 @@ import { Router } from 'express';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'Hello world 1' });
-});
+routes.get('/', (req, res) => res.json({ message: 'Hello world 1' }));
 
 export default routes;
