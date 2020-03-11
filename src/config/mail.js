017a65c32731028bb6fb60092ddee3073d7fc1f5
Felipe Maccheri <felipemaccheri@gmail.com>
2020-03-11 08:02:03 -0300

    Mailing and new insomnia json file

---
+++ src/config/mail.js
@@ -0,0 +1,19 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: 'ec2aeb510f2249',
+    pass: 'a8f6dbb0bbd7e3',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
+
+// Amazon SES
+// Mailgun
+// Sparkpost
+// Mandril (Mailchimp)
+
+// Mailtrap (DEV) https://mailtrap.io/
