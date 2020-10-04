5cc5704d177c212d7065c78390d6b173917eac25
Felipe Maccheri <felipemaccheri@gmail.com>
2020-10-04 15:50:17 -0300

    Adding cors lib as well as adjust some APIs to return needed data

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -2,6 +2,7 @@ import * as Yup from 'yup';
 import jwt from 'jsonwebtoken';
 
 import User from '../models/User';
+import File from '../models/File';
 import authConfig from '../../config/auth';
 
 class SessionController {
@@ -19,7 +20,16 @@ class SessionController {
 
     const { email, password } = req.body;
 
-    const user = await User.findOne({ where: { email } });
+    const user = await User.findOne({
+      where: { email },
+      include: [
+        {
+          model: File,
+          as: 'avatar',
+          attributes: ['id', 'path', 'url'],
+        },
+      ],
+    });
 
     if (!user) {
       return res.status(401).json({ error: 'User not found.' });
@@ -29,7 +39,7 @@ class SessionController {
       return res.status(401).json({ error: 'Password does not match.' });
     }
 
-    const { id, name } = user;
+    const { id, name, avatar, provider } = user;
 
     // https://www.md5online.org/
     return res.json({
@@ -37,6 +47,8 @@ class SessionController {
         id,
         name,
         email,
+        avatar,
+        provider,
       },
       token: jwt.sign({ id }, authConfig.secret, {
         expiresIn: authConfig.expiresIn,
